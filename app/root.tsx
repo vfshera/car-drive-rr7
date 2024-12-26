1630f87eab56d5703f4f73f3b9e800d11442ef03
Franklin Shera <fshera96@gmail.com>
2024-12-26 20:13:19 +0300

    fix: css oading

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,7 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: "./themify-icons.css" },
   { rel: "stylesheet", href: stylesheet },
 ];
 
